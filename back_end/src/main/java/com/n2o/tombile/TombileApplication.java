b667dd6c8bf19600d95e7fbd9be16062675794ad
aztenz <aztenz@outlook.com>
2024-02-17 23:18:42 +0200

    initiate environment

---
+++ back_end/src/main/java/com/n2o/tombile/TombileApplication.java
@@ -0,0 +1,13 @@
+package com.n2o.tombile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TombileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TombileApplication.class, args);
+	}
+
+}
---
+++ back_end/src/test/java/com/n2o/tombile/TombileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n2o.tombile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TombileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
