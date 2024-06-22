5a6a80bda1fc474ec89d45efac62d72091134928
Aztenz <aztenz@outlook.com>
2024-06-22 10:25:18 +0300

    fix: make quantity only for car parts

--- back_end/src/main/java/com/n2o/tombile/product/part/dto/RQPersistPart.java
+++ back_end/src/main/java/com/n2o/tombile/product/part/dto/RQPersistPart.java
@@ -1,11 +1,14 @@
 package com.n2o.tombile.product.part.dto;
 
 import com.n2o.tombile.product.product.dto.RQPersistProduct;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 
 import static com.n2o.tombile.core.common.util.Constants.ERROR_COMPATIBILITY_REQUIRED;
 import static com.n2o.tombile.core.common.util.Constants.ERROR_MANUFACTURER_REQUIRED;
+import static com.n2o.tombile.core.common.util.Constants.ERROR_QUANTITY_NEGATIVE;
+import static com.n2o.tombile.core.common.util.Constants.POSITIVE_NON_ZERO_NUM_MIN;
 
 @Getter
 public class RQPersistPart extends RQPersistProduct {
@@ -14,4 +17,7 @@ public class RQPersistPart extends RQPersistProduct {
 
     @NotEmpty(message = ERROR_COMPATIBILITY_REQUIRED)
     private String compatibility;
+
+    @Min(value = POSITIVE_NON_ZERO_NUM_MIN, message = ERROR_QUANTITY_NEGATIVE)
+    private int quantity;
 }
